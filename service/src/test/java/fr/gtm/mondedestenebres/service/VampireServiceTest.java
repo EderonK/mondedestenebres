884676a7a52c2a03a946fe79092fd38eef626d61
EderonK <nathandubot@gmail.com>
2018-07-14 04:34:21 +0200

    Multi module mis en place.

---
+++ service/src/test/java/fr/gtm/mondedestenebres/service/VampireServiceTest.java
@@ -0,0 +1,20 @@
+package fr.gtm.mondedestenebres.service;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+import fr.gtm.mondedestenebres.domaine.Vampire;
+import junit.framework.TestCase;
+
+//public class VampireServiceTest extends TestCase
+//{
+//	VampireService service = new VampireService();
+
+//	@Test public void testRecenserPopulation()
+//	{
+//		ArrayList<Vampire> vampires = service.recenserPopulation();
+//		assertEquals(vampires.size()>=0,true);
+//	}
+//}
